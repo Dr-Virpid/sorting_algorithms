1103a328596b6f83c6f5963b7172fc5ec9f44404
Dr-Virpid <ogunbonajesutofunmi@gmail.com>
2023-04-27 11:53:05 +0000

    added code for file 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,5 +9,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int temp, swap;
 
+	if (!array || size < 2))
+		return NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (swap < 1)
+			break;
+	}
 }
