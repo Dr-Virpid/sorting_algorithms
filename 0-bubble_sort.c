1075cf3ace12ad0809b633bf245ad85c5589904f
Dr-Virpid <ogunbonajesutofunmi@gmail.com>
2023-04-27 01:44:16 +0000

    added readme file and initial code

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using bubble sort
+ *
+ * @array: array of integers
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#Sorting Algorithms
+Alx Task on sorting algorithms and big O notation
