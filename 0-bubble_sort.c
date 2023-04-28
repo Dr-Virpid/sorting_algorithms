2bb49b871d3e87ab657c6f9acaa4349829ff9bb4
Dr-Virpid <ogunbonajesutofunmi@gmail.com>
2023-04-28 13:14:30 +0000

    fixed bug for bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,8 +12,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp, swap;
 
-	if (!array || size < 2))
-		return NULL;
+	if (!array || size < 2)
+		return ;
 
 	for (i = 0; i < size; i++)
 	{
